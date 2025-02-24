34d315a1be6981a8e60e1a55d48767b295ec5b72
K Tarun Reddy <tarunreddykreddy@gmail.com>
2025-02-24 15:36:05 +0530

    Added Single File and Integration Key parsing

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+let TSPLIntegrationKey = "";
+
+let intKeyAttr = document.querySelector('script[data-name="tsplscript"]')?.attributes.getNamedItem("data-integration-key");
+console.log(intKeyAttr)
+if (intKeyAttr !== undefined && intKeyAttr !== null){
+  TSPLIntegrationKey = intKeyAttr.value;
+  alert("Integration Key : "+TSPLIntegrationKey);
+}
+
+const newDiv = document.createElement('div');
+
+// Set some properties
+newDiv.id = 'root';
+
+// Append the new div to the body
+document.body.appendChild(newDiv);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
